ba6f3a6ea7c48a435c70aa7c25acabb114689e92
serem53 <trevorserem53@gmail.com>
2024-01-10 22:38:38 +0530

    logging and exception upddated

--- src/exception/exception.py
+++ src/exception/exception.py
@@ -1,13 +1,24 @@
 import sys
 
-class customerexception(Exception):
+#check sys module
+#customexception class inheriting from Exception class
+class customexception(Exception):
 
     def __init__(self,error_message,error_details:sys):
         self.error_message=error_message
         _,_,exc_tb=error_details.exc_info()
+        #tb = traceback line no
         self.lineno = exc_tb.tb_lineno
         self.file_name = exc_tb.tb_frame.f_code.co_filename
 
 
     def __str__(self):
-       pass
+       return "Error occured in python script [{0}] line number [{1}] error message [{2}],".format(
+       self.file_name,self.lineno,str(self.error_message))
+
+if __name__  == "__main__":
+    try:
+        a = 1/0
+        pass
+    except Exception as e:
+        raise customexception(e,sys)
---
+++ test.py
@@ -0,0 +1,4 @@
+from src.logger.logging import logging
+
+
+logging.info("this is my testing")
